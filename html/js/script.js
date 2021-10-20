1cb9b54599bf4c8ee37a9f2412e631d78038d3e1
Mr-Kobra <red.kras.2013@gmail.com>
2021-10-20 22:58:18 +0300

    Add tab script

--- html/js/script.js
+++ html/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function (){
+    /* Слайдер о нас на главной */
     $('.about-us-slider').slick({
         dots: true,
         infinite: false
+    })
+    /* Табы */
+    $('.dropdown-block.active').find('.dropdown-body').show()
+    $('.dropdown-header').on('click', function(e){
+        e.preventDefault()
+        $(this).parent().toggleClass('active')
+        $(this).parent().find('.dropdown-body').slideToggle(500)
     })
 })
