773e66c479f5de11584a8739b3096144bff3d794
Mr-Kobra <red.kras.2013@gmail.com>
2021-10-22 00:07:08 +0300

    Add adaptive styles before 768 px

--- html/js/script.js
+++ html/js/script.js
@@ -11,4 +11,28 @@ $(document).ready(function (){
         $(this).parent().toggleClass('active')
         $(this).parent().find('.dropdown-body').slideToggle(500)
     })
+    /* Мобильное меню */
+    /*$('.mobile-btn').on('click', function(){
+        $(this).toggleClass('active');
+        $('.top-nav').toggleClass('active');
+    })
+    function fixedSiteName(){
+        var width = $(window).width();
+        var siteNameHeight = $('.top-header-siteName').outerHeight();
+        if(width <= 992) {
+            $('.top-header-siteName').addClass('fix');
+            $('.top-header').css('padding-top', siteNameHeight);
+            $('.top-nav').css('padding-top', siteNameHeight);
+        } else {
+            $('.top-header-siteName').removeClass('fix');
+            $('.top-header').css('padding-top', 0);
+            $('.top-nav').css('padding-top', siteNameHeight);
+            $('.top-nav').removeClass('active')
+            $('.mobile-btn').removeClass('active')
+        }
+    }
+    fixedSiteName()
+    $(window).resize(function(){
+        fixedSiteName();
+    }) */
 })
