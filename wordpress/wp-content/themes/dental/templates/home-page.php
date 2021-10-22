c89999d759f3fdafe671a4b77770a2f80c99ce72
Mr-Kobra <red.kras.2013@gmail.com>
2021-10-22 20:33:39 +0300

    Add about us block

--- wordpress/wp-content/themes/dental/templates/home-page.php
+++ wordpress/wp-content/themes/dental/templates/home-page.php
@@ -47,6 +47,52 @@ get_header();
     <?php endif;
     wp_reset_postdata(); ?>
 
+    <?php if(get_field('about_us_heading') || get_field('about_us_info') || get_field('about_us_slider')): ?>
+    <div class="about-us">
+        <div class="container">
+            <?php if(get_field('about_us_heading')): ?>
+            <div class="heading">
+                <?php the_field('about_us_heading'); ?>
+            </div>
+            <?php endif; ?>
+            <div class="about-us-container">
+                <div class="about-us-text">
+                    <?php if(get_field('about_us_info')) {
+                        the_field('about_us_info');
+                    } ?>
+                    <div class="about-us-chief">
+                        <?php
+                        $img = get_field('about_us_img');
+                        if($img):
+                        ?>
+                        <div class="about-us-chief-img">
+                            <img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>">
+                        </div>
+                        <?php endif; ?>
+                        <?php if(get_field('about_us_chief')): ?>
+                        <div class="about-us-chief-info">
+                            <?php the_field('about_us_chief'); ?>
+                        </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <?php if(get_field('about_us_slider')): ?>
+                <div class="about-us-slider">
+                    <?php while(has_sub_field('about_us_slider')): ?>
+                    <div class="about-us-slide">
+                        <?php $img = get_sub_field('img'); ?>
+                        <div class="about-us-slide-img" style="background: url(<?php echo $img['url']; ?>) no-repeat center center">
+                            <img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>">
+                        </div>
+                    </div>
+                    <?php endwhile; ?>
+                </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+
 <?php
 get_footer();
 
