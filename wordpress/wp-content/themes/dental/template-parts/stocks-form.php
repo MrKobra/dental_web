689f1de9ddf7840656b028b07ced60c6a62ba294
Mr-Kobra <red.kras.2013@gmail.com>
2021-10-25 20:39:45 +0300

    Add edits (25.10.2021) v 1.1

--- wordpress/wp-content/themes/dental/template-parts/stocks-form.php
+++ wordpress/wp-content/themes/dental/template-parts/stocks-form.php
@@ -1,15 +1,14 @@
+<?php if(get_field('stocks_text') || get_field('stocks_img')): ?>
 <div class="stocks-form form-block">
     <div class="container">
-        <div class="form-block-container">
+        <?php $img = get_field('stocks_img'); ?>
+        <div class="form-block-container" style="background: url(<?php echo $img['url']; ?>) no-repeat center right">
             <div class="form-block-text">
-                <h2>Имплантация <span>ALL-ON-2</span> под ключ</h2>
-                <p>Восстановите все зубы и получите звездную улыбку по доступной цене.<br>
-                    Укажите свой номер<br>
-                    или позвоните нам сами — <a href="tel:<?php the_field('sign_phone', 'options'); ?>"><?php the_field('sign_phone', 'options'); ?></a></p>
-                <p>
+                <?php the_field('stocks_text'); ?>
                 <?php echo do_shortcode('[contact-form-7 id="103" title="Акционная форма"]'); ?>
                 <span>Записываясь на услугу вы даёте согласие на <a href="<?php echo get_page_link(3);?>">обработку своих персональных данных</a></span>
             </div>
         </div>
     </div>
 </div>
+<?php endif; ?>
