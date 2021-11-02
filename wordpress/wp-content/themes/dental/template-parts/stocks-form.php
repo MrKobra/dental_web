69523f89bef7e2a0b5148b62034f09fd8e4cd914
Mr-Kobra <red.kras.2013@gmail.com>
2021-11-02 12:27:31 +0300

    Add stocks form img on mobile (02.11.21)

--- wordpress/wp-content/themes/dental/template-parts/stocks-form.php
+++ wordpress/wp-content/themes/dental/template-parts/stocks-form.php
@@ -2,7 +2,10 @@
 <div class="stocks-form form-block">
     <div class="container">
         <?php $img = get_field('stocks_img'); ?>
-        <div class="form-block-container" style="background: url(<?php echo $img['url']; ?>) no-repeat center right">
+        <div class="form-block-container">
+            <div class="form-block-img">
+                <img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>">
+            </div>
             <div class="form-block-text">
                 <?php the_field('stocks_text'); ?>
                 <?php echo do_shortcode('[contact-form-7 id="103" title="Акционная форма"]'); ?>
